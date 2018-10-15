5b55b2df5244608f7808547bc9b6c64db63b1f69
Mitali Salvi <42700383+mitalisalvi@users.noreply.github.com>
2018-10-15 19:20:45 -0400

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,36 @@
+package com;
+
+public class ReverseString 
+{
+	public static void main(String[] args) 
+	{
+		String s ="abcd";
+		int k =2;
+		System.out.println(reverseStr(s, k));
+		
+	}
+	
+	public static String reverseStr(String s, int k) 
+	{
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) 
+        {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i = i+ 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private static void swap(char[] arr, int l, int r) 
+    {
+        while (l < r) 
+        {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+
+}
